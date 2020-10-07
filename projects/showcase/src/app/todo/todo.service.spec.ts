06c993927b18bef0278d86a4c3052657214cceb9
dingnan <nandingb@gmail.com>
2020-10-07 10:03:52 -0400

    add todo service and unit test

--- projects/showcase/src/app/todo/todo.service.spec.ts
+++ projects/showcase/src/app/todo/todo.service.spec.ts
@@ -1,16 +1,63 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TodoService } from './todo.service';
+
+// https://jestjs.io/docs/en/setup-teardown
+// Applies to all tests in this file
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+  });
+});
 
 describe('TodoService', () => {
   let service: TodoService;
+  let httpMock: HttpTestingController;
 
+  // Applies only to tests in this describe block
   beforeEach(() => {
-    TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  // another way to inject
+  // beforeEach(inject([TodoService, HttpTestingController], (srv, http) => {
+  //   service = srv;
+  //   httpMock = http;
+  // }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should throw an error if createTodo called with null object', () => {
+    // must wrap the code in a function, otherwise the error will not be caught and assertion will fail.
+    expect(() => {
+      service.createTodo(null);
+    }).toThrow('empty todo');
+  });
+
+  it('should be able to get todos', () => {
+    const expectedResponse = [
+      { userId: 1, id: 1, title: 'delectus aut autem', completed: false },
+    ];
+
+    service.getTodos().subscribe((res) => {
+      expect(res).toEqual(expectedResponse);
+    });
+
+    // assert that one request was made to the url
+    const mockReq = httpMock.expectOne(service.baseUrl + 'todos');
+    // assert that the request hasn't been cancelled and the response is of type json
+    expect(mockReq.cancelled).toBeFalsy();
+    expect(mockReq.request.responseType).toEqual('json');
+    // assert that request method is GET
+    expect(mockReq.request.method).toEqual('GET');
+
+    mockReq.flush(expectedResponse);
+    httpMock.verify();
   });
 });
--- projects/showcase/src/app/todo/todo.service.ts
+++ projects/showcase/src/app/todo/todo.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+// A service becomes singleton when we apply providedIn
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  readonly baseUrl = 'https://jsonplaceholder.typicode.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /* 
+  POST – Create
+  GET – Read/Retrieve
+  PUT/PATCH – Update
+  DELETE – Delete
+  */
+  getTodos() {
+    return this.http.get(this.baseUrl + 'todos');
+  }
+
+  getTodo(id) {
+    if (!id) throw 'empty id';
+  }
+
+  createTodo(todo) {
+    if (!todo) throw 'empty todo';
+  }
+
+  // PUT overwrites the entire entity if it already exists, and creates a new resource if it doesn’t exist.
+  updateTodo(todo) {
+    if (!todo) throw 'empty todo';
+  }
+
+  // PATCH is used when you want to apply a partial update to the resource.
+  updateTodoCompleteStatus(id, completed) {
+    if (!id) throw 'empty todo id';
+  }
+
+  deleteTodo(id) {
+    if (!id) throw 'empty todo id';
+  }
 }
