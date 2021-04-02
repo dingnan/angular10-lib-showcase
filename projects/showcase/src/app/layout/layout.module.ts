625b031132c634ff9c5b1889188b00096745f752
dingnan <nandingb@gmail.com>
2021-04-02 13:14:27 -0400

    adjust footer style

--- projects/showcase/src/app/layout/layout.module.ts
+++ projects/showcase/src/app/layout/layout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
 import { HeaderComponent } from './header/header.component'
 import { FooterComponent } from './footer/footer.component'
 import { NavComponent } from './nav/nav.component'
@@ -7,7 +8,7 @@ import { SharedModule } from '../modules/shared/shared.module'
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, NavComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [HeaderComponent, FooterComponent, NavComponent],
 })
 export class LayoutModule {}
