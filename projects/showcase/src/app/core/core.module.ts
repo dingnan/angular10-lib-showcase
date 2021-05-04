3617b87ff0e74c6c303368fa85178dcdc8a1a735
dingnan <nandingb@gmail.com>
2021-05-04 16:24:59 -0400

    move logging service to core module

--- projects/showcase/src/app/core/core.module.ts
+++ projects/showcase/src/app/core/core.module.ts
@@ -1,7 +1,9 @@
 import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core'
 import { Optional } from '@angular/core';
 import { SkipSelf } from '@angular/core';
+import { LOGGER_SETTING, LoggingLevel } from '../modules/shared/model/log.settings';
 import { AppConfigService } from './services/config/app-config.service';
+import { LoggerService } from './services/logger/logger.service';
 
 const appInitializerFunc = (appConfig: AppConfigService) => {
   return () => {
@@ -19,7 +21,7 @@ export class CoreModule {
     }
   }
 
-  static forRoot(): ModuleWithProviders<CoreModule> {
+  static forRoot(config?: LoggingLevel): ModuleWithProviders<CoreModule> {
     return {
       ngModule: CoreModule,
       providers: [
@@ -28,6 +30,15 @@ export class CoreModule {
           useFactory: appInitializerFunc,
           multi: true,
           deps: [AppConfigService]
+        },
+        {
+          provide: LOGGER_SETTING,
+          useValue: config ?? LoggingLevel.all // if no config passed, use default settings}
+        },
+        {
+          provide: LoggerService, 
+          useClass: LoggerService, 
+          deps: [LOGGER_SETTING]
         }
       ]
     }
--- projects/showcase/src/app/modules/shared/shared.module.ts
+++ projects/showcase/src/app/modules/shared/shared.module.ts
@@ -4,8 +4,6 @@ import { AngularMaterialModule } from './material.module';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { UserComponent } from './components/user/user.component';
 import { SpinnerOverlayService } from './services/spinner/spinner-overlay.service';
-import { LOGGER_SETTING, LoggingLevel } from './model/log.settings';
-import { LoggerService } from '../../core/services/logger/logger.service';
 
 @NgModule({
   declarations: [PageNotFoundComponent, UserComponent],
@@ -13,13 +11,10 @@ import { LoggerService } from '../../core/services/logger/logger.service';
   exports: [AngularMaterialModule, PageNotFoundComponent, UserComponent],
 })
 export class SharedModule {
-  static forRoot(config?: LoggingLevel): ModuleWithProviders<SharedModule> {
+  static forRoot(): ModuleWithProviders<SharedModule> {
     return {
       ngModule: SharedModule,
-      providers: [ 
-        {provide: LOGGER_SETTING, useValue: config ?? LoggingLevel.all}, // if no config passed, use default settings
-        SpinnerOverlayService, 
-        {provide: LoggerService, useClass: LoggerService, deps: [LOGGER_SETTING]} ]
+      providers: [ SpinnerOverlayService ]
     }
   }
 }
