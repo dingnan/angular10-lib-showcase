b30d5bfbb6c88d868ee83bd080d76162f6b218fe
dingnan <nandingb@gmail.com>
2021-05-17 14:59:08 -0400

    update nav component code

--- projects/showcase/src/app/layout/nav/nav.component.ts
+++ projects/showcase/src/app/layout/nav/nav.component.ts
@@ -1,42 +1,63 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy } from '@angular/core'
+import { NavigationEnd, Router, RouterEvent } from '@angular/router'
+import { Subject } from 'rxjs'
+import { filter, takeUntil } from 'rxjs/operators'
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnDestroy {
   navLinks = [
     {
       label: 'Home',
-      path: './',
+      path: '/',
       index: 0,
     },
     {
       label: 'Alert Lib',
-      path: './alert',
+      path: '/alert',
       index: 1,
     },
     {
       label: 'Flex layout',
-      path: './flex',
+      path: '/flex',
       index: 2,
     },
     {
       label: 'To do',
-      path: './todo',
+      path: '/todo',
       index: 3,
     },
     {
       label: 'Checkout',
-      path: './checkout',
+      path: '/checkout',
       index: 4,
     },
   ]
 
   activeLink = this.navLinks[0]
 
-  constructor() {}
+  destroy$ = new Subject<any>()
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {
+    router.events
+      .pipe(
+        filter(e => e instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((val: RouterEvent) => {
+        if (val) {
+          const link = this.navLinks.find(n => n.path === val.url)
+          if (link) {
+            this.activeLink = link;
+          }
+        }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
 }
